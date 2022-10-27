d5bd1084280535266d6c689ac71cefa53cdaf211
Stefan Thurnherr <stefanthurnherr@gmail.com>
2022-10-27 23:04:50 +0200

    Add endpoint to calculate slideshow checksum

--- src/main/java/org/bondor/dashboard/DashboardSlideshow.java
+++ src/main/java/org/bondor/dashboard/DashboardSlideshow.java
@@ -1,20 +1,25 @@
 package org.bondor.dashboard;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import com.drew.metadata.Directory;
-import com.drew.metadata.Tag;
+import java.math.BigInteger;
 import java.net.URI;
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.jpeg.JpegMetadataReader;
-import com.drew.metadata.Metadata;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Comparator;
+
+import com.drew.metadata.Directory;
+import com.drew.metadata.Tag;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.metadata.Metadata;
 
 import org.springframework.core.io.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,8 +67,42 @@ public class DashboardSlideshow {
 
     model.addAttribute("intervalMsecs", 3000);
 
+    model.addAttribute("slideshowChecksum", calculateChecksum(resources));
+
     return "slideshowTemplate";
   }
+
+  @GetMapping(value = "/slideshowChecksum")
+  public String slideshowChecksum(final Model model) throws Exception {
+    final List<Resource> resources = new DashboardApplication().allImageResources();
+    return calculateChecksum(resources);
+  }
+
+  private static String calculateChecksum(List<Resource> resources) throws Exception {
+    final MessageDigest shaDigest = MessageDigest.getInstance("SHA3-256");
+
+    byte[] byteArray = new byte[1024];
+    int bytesCount = 0;
+    for (final Resource resource : resources) {
+      try (InputStream inputStream = resource.getInputStream()) {
+        while ((bytesCount = inputStream.read(byteArray)) != -1) {
+          shaDigest.update(byteArray, 0, bytesCount);
+        };
+      }
+    }
+
+    final String hashText = convertToHex(shaDigest.digest());
+    return hashText;
+  }
+
+  private static String convertToHex(final byte[] bytes) {
+      final BigInteger bigint = new BigInteger(1, bytes);
+      String hexText = bigint.toString(16);
+      while (hexText.length() < 32) {
+         hexText = "0".concat(hexText);
+      }
+      return hexText;
+   }
 
   private void sortByExifCreatedDate(List<Resource> unsortedResources) throws Exception {
     for (final Resource resource : unsortedResources) {
