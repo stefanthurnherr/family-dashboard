08df8b539858dbb8d207e2fce8710b1c8435e84f
Stefan Thurnherr <stefan.thurnherr+github@gmail.com>
2024-01-03 18:49:51 +0100

    Update receive-messages.py
    Fix variable name typo

--- signal-cli-client/receive-messages.py
+++ signal-cli-client/receive-messages.py
@@ -20,7 +20,7 @@ import re
 
 BASE_FILEPATH = '/home/pi/signal-client/'
 CONFIG_FILEPATH = BASE_FILEPATH + 'my.cfg'
-RECEIVED_MESSAGES_LOG_FILE_PATH = BASE_FILE_PATH + 'received-messages.txt'
+RECEIVED_MESSAGES_LOG_FILE_PATH = BASE_FILEPATH + 'received-messages.txt'
 VERSION = '0.28'
 DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
